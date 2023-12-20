7e2b71c7f3e3a30ad73e53b6562241669297e2ba
faresZlaci <133266884+faresZlaci@users.noreply.github.com>
2023-12-20 11:11:24 +0100

    Update database.js

--- NodeJS-Login-page---MongoDB-master/config/database.js
+++ NodeJS-Login-page---MongoDB-master/config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://upjjke4vjonsgdmfljvz:W0RMlS83QL0sBRLCfqzI@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bw89yjrz0r2twmc?replicaSet=rs0' 
+	'url' : 'mongodb://uvrqiwahjpcsextetpun:uWqPP5rOJWNeFuAPaq07@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/b4x4mcqkjdboq4o?replicaSet=rs0' 
 
 };
